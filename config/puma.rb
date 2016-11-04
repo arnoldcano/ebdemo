54ce3a182b33986b051972110d30dcd7048c6b26
Arnold Cano <arnoldcano@yahoo.com>
2016-11-04 11:37:38 -0500

    fix puma binding

--- config/puma.rb
+++ config/puma.rb
@@ -45,5 +45,3 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-bind 'tcp://0.0.0.0'
--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ if [ "$RAILS_ENV" = "production" ]; then
   bundle exec rake assets:precompile
 fi
 
-bundle exec puma -C config/puma.rb
+#bundle exec puma -C config/puma.rb
+bundle exec rails s -b 0.0.0.0
