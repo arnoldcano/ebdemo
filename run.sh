67a88d2e1bbf8a5cb7a68f309c6d2cb0fc2ca7d6
Arnold Cano <arnoldcano@yahoo.com>
2016-11-04 13:33:34 -0500

    try puma again

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ if [ "$RAILS_ENV" = "production" ]; then
   bundle exec rake assets:precompile
 fi
 
-#bundle exec puma -C config/puma.rb
-bundle exec rails s -b 0.0.0.0
+bundle exec puma -C config/puma.rb
